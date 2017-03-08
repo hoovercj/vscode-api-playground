a9384e99b8d238a2390702b606b10c6f53c4233f
Cody Hoover <git@codyhoover.com>
2017-03-08 23:21:15 +0100

    Change extension so it contributes a command to open a new file with the configured language mode

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,17 @@
 'use strict';
-import { ExtensionContext } from 'vscode';
-import { SymbolKindIconPreviewDocumentSymbolProvider } from './symbolKindIconPreviewDocumentSymbolProvider';
+import { ExtensionContext, workspace, window, commands } from 'vscode';
 
 let ctx: ExtensionContext;
 
 export function activate(context: ExtensionContext) {
     ctx = context;
-    const symbolKindPreviewDocumentSymbolProvider = new SymbolKindIconPreviewDocumentSymbolProvider(context);
-    symbolKindPreviewDocumentSymbolProvider.register();
+    const command = commands.registerCommand('editor.newFile.withLanguageMode', () => {
+        const language = workspace.getConfiguration('editor.newFile').get('defaultLanguageMode', null);
+
+        const options = language ? {language} : null;
+        workspace.openTextDocument(options).then((document) => window.showTextDocument(document));
+    });
+    ctx.subscriptions.push(command);
 }
 
 // this method is called when your extension is deactivated
