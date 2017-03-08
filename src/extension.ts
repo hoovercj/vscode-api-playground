d6270ddebb5b2a86646469bb20b84242f9e5ed83
Cody Hoover <git@codyhoover.com>
2017-03-08 22:50:51 +0100

    Change extension so it always focuses on active editor and collapses other folders

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,18 @@
 'use strict';
-import { ExtensionContext } from 'vscode';
-import { SymbolKindIconPreviewDocumentSymbolProvider } from './symbolKindIconPreviewDocumentSymbolProvider';
+import { ExtensionContext, commands, workspace, window } from 'vscode';
 
 let ctx: ExtensionContext;
 
 export function activate(context: ExtensionContext) {
     ctx = context;
-    const symbolKindPreviewDocumentSymbolProvider = new SymbolKindIconPreviewDocumentSymbolProvider(context);
-    symbolKindPreviewDocumentSymbolProvider.register();
+
+    window.onDidChangeActiveTextEditor(() => {
+        commands.executeCommand('workbench.files.action.collapseExplorerFolders').then(() => {
+            commands.executeCommand('workbench.files.action.showActiveFileInExplorer');
+        }).then(() => {
+            commands.executeCommand('workbench.action.focusActiveEditorGroup');
+        });
+    });
 }
 
 // this method is called when your extension is deactivated
